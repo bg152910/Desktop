99880b5c2bbcb5936caefb3d0373d8f455508671
Brandon Guerrero <brandong152910@icloud.com>
2023-06-15 11:23:46 -0700

    Initial Commit

---
+++ 100_Days_Of_Code.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+//Variables strings and integers
+var greeting = "Hello, playground"
+var age = 28
+var population = 8_000_000
+
+//Multi-line comments
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+//Double
+var pi = 3.141
+
+//Boolean
+var awesome = true
+
+//String interperlation
+var score = 85
+var str = "Your score was \(score)"
+var results = "The test results are here: \(str)"
+
+//Constant value will never change
+let taylor = "swift"
+
+//Constant assigning type of value 
+let str = "Hello, playground"
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
---
+++ 100_Days_Of_Code.playground/Pages/sets.xcplaygroundpage/Contents.swift
@@ -0,0 +1,5 @@
+//: [Previous](@previous)
+
+import Foundation
+
+let colors = Set(["red", "green", "blue"])
